77de0af80a8e109a31004332557a819707969e78
NavehShovali <navehshovali@campus.technion.ac.il>
2022-02-15 16:40:17 +0200

    Assemble the hub-spoke architecture

---
+++ main.tf
@@ -0,0 +1,99 @@
+locals {
+  resource_prefix = "naveh-tf-final"
+}
+
+resource "azurerm_resource_group" "hub" {
+  location = var.location
+  name     = "${local.resource_prefix}-hub"
+}
+
+resource "azurerm_resource_group" "spoke" {
+  location = var.location
+  name     = "${local.resource_prefix}-spoke"
+}
+
+module "hub_virtual_network" {
+  source = "./virtual_network"
+
+  address_space       = var.hub_virtual_network.address_space
+  location            = var.location
+  name                = "${local.resource_prefix}-${var.hub_virtual_network.name}"
+  resource_group_name = azurerm_resource_group.hub.name
+  subnets             = concat([
+    var.virtual_private_network_gateway.subnet, var.firewall.subnet
+  ], var.hub_virtual_network.subnets)
+}
+
+locals {
+  gateway_subnet_id  = module.hub_virtual_network.subnets[0].id
+  firewall_subnet_id = module.hub_virtual_network.subnets[1].id
+}
+
+module "hub_virtual_private_network_gateway" {
+  source = "./virtual_private_network_gateway"
+
+  address_prefixes                      = var.virtual_private_network_gateway.address_prefixes
+  azure_active_directory_authentication = var.virtual_private_network_gateway.azure_active_directory_authentication
+  location                              = var.location
+  name                                  = "${local.resource_prefix}-${var.virtual_private_network_gateway.name}"
+  resource_group_name                   = azurerm_resource_group.hub.name
+  subnet_id                             = local.gateway_subnet_id
+}
+
+module "hub_firewall" {
+  source = "./firewall"
+
+  location                      = var.location
+  name                          = "${local.resource_prefix}-${var.firewall.name}"
+  policy_rule_collection_groups = var.firewall.policy_rule_collection_groups
+  resource_group_name           = azurerm_resource_group.hub.name
+  subnet_id                     = local.firewall_subnet_id
+}
+
+module "spoke_virtual_network" {
+  source = "./virtual_network"
+
+  address_space       = var.spoke_virtual_network.address_space
+  location            = var.location
+  name                = "${local.resource_prefix}-${var.spoke_virtual_network.name}"
+  resource_group_name = azurerm_resource_group.spoke.name
+  subnets             = var.spoke_virtual_network.subnets
+}
+
+locals {
+  spoke_subnet_id = module.spoke_virtual_network.subnets[0].id
+}
+
+module "spoke_network_security_group" {
+  source = "./network_security_group"
+
+  location            = var.location
+  name                = "${local.resource_prefix}-${var.hub_network_security_group}"
+  resource_group_name = azurerm_resource_group.spoke.name
+  security_rules      = var.hub_network_security_group.security_rules
+}
+
+resource "azurerm_subnet_network_security_group_association" "spoke" {
+  network_security_group_id = module.spoke_network_security_group
+  subnet_id                 = local.spoke_subnet_id.id
+}
+
+module "spoke_virtual_machine" {
+  source = "./virtual_machine"
+
+  location                = var.location
+  name                    = "${local.resource_prefix}-${var.spoke_virtual_machine.name}"
+  os_profile              = var.spoke_virtual_machine.os_profile
+  resource_group_name     = azurerm_resource_group.spoke.name
+  storage_image_reference = var.spoke_virtual_machine.storage_image_reference
+  subnet_id               = local.spoke_subnet_id
+  vm_size                 = var.spoke_virtual_machine.vm_size
+}
+
+module "spoke_storage_account" {
+  source = "./storage_account"
+
+  location            = var.location
+  name                = var.spoke_storage_account.name
+  resource_group_name = azurerm_resource_group.spoke.name
+}
---
+++ variables.tf
@@ -0,0 +1,120 @@
+variable "location" {
+  description = "Location to deploy to"
+  type        = string
+}
+
+variable "hub_virtual_network" {
+  description = "The virtual network to deploy to the hub"
+  type        = object({
+    name          = string
+    address_space = list(string)
+    subnets       = list(object({
+      name             = string
+      address_prefixes = list(string)
+    }))
+  })
+}
+
+variable "spoke_virtual_network" {
+  description = "The virtual network to deploy to the spoke"
+  type        = object({
+    name          = string
+    address_space = list(string)
+    subnets       = list(object({
+      name             = string
+      address_prefixes = list(string)
+    }))
+  })
+}
+
+variable "virtual_private_network_gateway" {
+  description = "The virtual private network to deploy to the hub"
+  type        = object({
+    name             = string
+    address_prefixes = list(string)
+    subnet           = object({
+      name             = string
+      address_prefixes = list(string)
+    })
+    azure_active_directory_authentication = object({
+      audience = string
+      issuer   = string
+      tenant   = string
+    })
+  })
+}
+
+variable "firewall" {
+  description = "The firewall to deploy to the hub"
+  type        = object({
+    name   = string
+    subnet = object({
+      name             = string
+      address_prefixes = list(string)
+    })
+    policy_rule_collection_groups = map(object({
+      priority                 = number
+      network_rule_collections = map(object({
+        action   = string
+        name     = string
+        priority = number
+        rules    = map(object({
+          name                  = string
+          protocols             = list(string)
+          source_addresses      = list(string)
+          destination_addresses = list(string)
+          destination_ports     = list(string)
+        }))
+      }))
+    }))
+  })
+}
+
+variable "hub_network_security_group" {
+  description = "Defines the hub's network security group"
+  type        = object({
+    name           = string
+    security_rules = list(object({
+      access                     = string
+      direction                  = string
+      name                       = string
+      priority                   = number
+      protocol                   = string
+      source_port_range          = string
+      destination_port_range     = string
+      source_address_prefix      = string
+      destination_address_prefix = string
+    }))
+  })
+}
+
+variable "virtual_machine_password" {
+  description = "Default password value of a virtual machine admin user"
+  type        = string
+  sensitive   = true
+}
+
+variable "spoke_virtual_machine" {
+  description = "The virtual machine to deploy to the spoke"
+  type        = object({
+    name                    = string
+    vm_size                 = string
+    storage_image_reference = object({
+      publisher = string
+      offer     = string
+      sku       = string
+      version   = string
+    })
+    os_profile = object({
+      computer_name  = string
+      admin_username = string
+    })
+  })
+}
+
+variable "spoke_storage_account" {
+  description = "Defines the storage account to deploy to the spoke"
+  type        = object({
+    name = string
+  })
+}
