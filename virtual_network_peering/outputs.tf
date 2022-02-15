7680a34c3d352d77e8a27f0c043732272f3cbad5
NavehShovali <navehshovali@campus.technion.ac.il>
2022-02-15 16:39:22 +0200

    Add outputs

---
+++ virtual_network_peering/outputs.tf
@@ -0,0 +1,11 @@
+output "id" {
+  value = azurerm_virtual_network_peering.peering.id
+}
+
+output "name" {
+  value = azurerm_virtual_network_peering.peering.name
+}
+
+output "data" {
+  value = azurerm_virtual_network_peering.peering
+}
