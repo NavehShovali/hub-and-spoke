e318e624e165b5ff78a4c4c0f4e8dcef1e1b2059
NavehShovali <navehshovali@campus.technion.ac.il>
2022-02-20 15:37:20 +0200

    Add support to parameters with multiple ranges or prefixes

--- modules/network_security_group/main.tf
+++ modules/network_security_group/main.tf
@@ -6,15 +6,19 @@ resource "azurerm_network_security_group" "security_group" {
   dynamic "security_rule" {
     for_each = var.security_rules
     content {
-      access                     = security_rule.value.access
-      direction                  = security_rule.value.direction
-      name                       = security_rule.key
-      priority                   = security_rule.value.priority
-      protocol                   = security_rule.value.protocol
-      source_port_range          = security_rule.value.source_port_range
-      destination_port_range     = security_rule.value.destination_port_range
-      source_address_prefix      = security_rule.value.source_address_prefix
-      destination_address_prefix = security_rule.value.destination_address_prefix
+      access                       = security_rule.value.access
+      direction                    = security_rule.value.direction
+      name                         = security_rule.key
+      priority                     = security_rule.value.priority
+      protocol                     = security_rule.value.protocol
+      source_port_range            = security_rule.value.source_port_range
+      destination_port_range       = security_rule.value.destination_port_range
+      source_address_prefix        = security_rule.value.source_address_prefix
+      destination_address_prefix   = security_rule.value.destination_address_prefix
+      source_port_ranges           = security_rule.value.source_port_ranges
+      destination_port_ranges      = security_rule.value.destination_port_ranges
+      source_address_prefixes      = security_rule.value.source_address_prefixes
+      destination_address_prefixes = security_rule.value.destination_address_prefixes
     }
   }
 }
--- modules/network_security_group/variables.tf
+++ modules/network_security_group/variables.tf
@@ -14,15 +14,19 @@ variable "location" {
 }
 
 variable "security_rules" {
-  type = map(object({
-    access                     = string
-    direction                  = string
-    priority                   = number
-    protocol                   = string
-    source_port_range          = string
-    destination_port_range     = string
-    source_address_prefix      = string
-    destination_address_prefix = string
+  description = "Network security rules. Note that if a range/prefix parameter is null, its corresponding ranges/prefixes parameter must not be null, and vice versa."
+  type        = map(object({
+    access                       = string
+    direction                    = string
+    priority                     = number
+    protocol                     = string
+    source_port_range            = string
+    destination_port_range       = string
+    source_address_prefix        = string
+    destination_address_prefix   = string
+    source_port_ranges           = list(string)
+    destination_port_ranges      = list(string)
+    source_address_prefixes      = list(string)
+    destination_address_prefixes = list(string)
   }))
-  description = "Network security rules"
 }
