61deb76be4fa1b70587b2aee31b07b225627ea4f
NavehShovali <navehshovali@campus.technion.ac.il>
2022-02-24 16:04:06 +0200

    Implement logs analytics workspace

---
+++ modules/logs_analytics_workspace/variables.tf
@@ -0,0 +1,24 @@
+variable "resource_group_name" {
+  description = "Resource group to deploy to"
+  type        = string
+}
+
+variable "location" {
+  description = "Location to deploy to"
+  type        = string
+}
+
+variable "name" {
+  description = "The name of the workspace"
+  type        = string
+}
+
+variable "sku" {
+  description = "Specifies the Sku of the Log Analytics Workspace. Defaults to 'PerGB2018'"
+  default     = "PerGB2018"
+}
+
+variable "retention_in_days" {
+  description = "The workspace data retention in days. Defaults to 30"
+  default     = 30
+}
